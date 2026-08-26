203117365594f45b57357d80b9fa3503b3d870d0
SLEDZARI <sharecraftwideo@gmail.com>
2026-08-27 01:01:16 +0200

    Remove obsolete Home compatibility entrypoint

--- site-v1.js
+++
@@ -1,2 +0,0 @@
-// Compatibility entrypoint retained by Home markup.
-if(!document.querySelector('script[src="/motion-v1.js"]')){const s=document.createElement('script');s.src='/motion-v1.js';s.defer=true;document.head.appendChild(s);}
